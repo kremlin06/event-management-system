8f3a9a96247852dbd3399630b0cc802d574bfce8
Kremlin <jhonkenth1307@gmail.com>
2026-06-16 12:48:12 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-// to bring back the "Something went wrong" error screen:
-//   1. uncomment the import below
-//   2. uncomment the <ErrorBoundary> wrapper in root.render()
-//
-// import ErrorBoundary from './components/ErrorBoundary.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+
+
+
+
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    {/* <ErrorBoundary> */}
-      <App />
-    {/* </ErrorBoundary> */}
+    {}
+    <App />
+    {}
   </StrictMode>
 );
